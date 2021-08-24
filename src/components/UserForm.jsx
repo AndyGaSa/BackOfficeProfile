5db00fd78e5bfb2cef4774345453ca8c8a10ff24
“Andy <garcia.andy.tecnologies10@gmail.com>
2021-08-24 23:16:11 +0200

    Add a material form that checks if the text is bigger than 2 and send error

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TextField from '@material-ui/core/TextField';
 import './UserForm.css';
 
 export default function UserForm() {
+  const [title, setTitle] = useState('');
+  const [legend, setLegend] = useState('');
+  const [errorTitle, setErrorTitle] = useState(false);
   return (
-    <form className="UserForm" />
+    <form className="UserForm">
+      <TextField
+        onChange={(event) => {
+          setTitle(event.target.value);
+          if (title.length > 2) {
+            setErrorTitle(true);
+            setLegend('Username is bigger than 2');
+          } else {
+            setErrorTitle(false);
+          }
+        }}
+        error={errorTitle}
+        label="Username"
+        helperText={legend}
+        variant="outlined"
+      />
+    </form>
   );
 }
