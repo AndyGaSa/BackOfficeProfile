0017ce8c606058cc94fc318c52e3d91f0437996e
“Andy <garcia.andy.tecnologies10@gmail.com>
2021-08-24 19:22:23 +0200

    Import Material Header
    -Change menu icon to one more accurate
    -Change title of the header
    -Install material icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import AccountBoxIcon from '@material-ui/icons/AccountBox';
 
-function Header() {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+}));
+
+export default function Header() {
+  const classes = useStyles();
+
   return (
-    <div>
-      <h1>Header</h1>
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar variant="dense">
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <AccountBoxIcon />
+          </IconButton>
+          <Typography variant="h6" color="inherit">
+            My Profile
+          </Typography>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }
-
-export default Header;
