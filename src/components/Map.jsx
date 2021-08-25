6cdd3c1e89a4b3c04010865de137fa721e51351c
“Andy <garcia.andy.tecnologies10@gmail.com>
2021-08-25 09:09:16 +0200

    Add google maps map with default location

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,20 +1,41 @@
-import React from 'react';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
+/* eslint-disable react/prop-types */
+import React, { useState } from 'react';
+import { CssBaseline, Typography, Container } from '@material-ui/core';
+import GoogleMapReact from 'google-map-react';
 
 export default function Map() {
+  const [lat, setLat] = useState(41.410167);
+  const [lng, setLgn] = useState(2.209742);
   function getCurrentPosition() {
     navigator.geolocation.getCurrentPosition((position) => {
-      alert(`latitud:${position.coords.latitude}, longitud:${position.coords.longitude}`);
+      setLat(position.coords.latitude);
+      setLgn(position.coords.longitude);
     });
   }
+  const defaultProps = {
+    center: {
+      lat,
+      lng,
+    },
+    zoom: 18,
+  };
+  const AnyReactComponent = ({ text }) => <div>{text}</div>;
   return (
     <>
       <CssBaseline />
       <Container maxWidth="md">
         <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '60vh' }}>
-          <div id="map" />
+          <GoogleMapReact
+            bootstrapURLKeys={{ key: 'AIzaSyBRUMLTzjuEywhAXLLpHwwOBTeYR0Biu-0' }}
+            defaultCenter={defaultProps.center}
+            defaultZoom={defaultProps.zoom}
+          >
+            <AnyReactComponent
+              lat={lat}
+              lng={lng}
+              text="My Marker"
+            />
+          </GoogleMapReact>
           <button type="button" onClick={getCurrentPosition}>pulsame</button>
         </Typography>
       </Container>
