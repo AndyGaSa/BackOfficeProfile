e3eced9196c67e065ecc803615fd42455de5e053
“Andy <garcia.andy.tecnologies10@gmail.com>
2021-08-25 08:52:08 +0200

    Add elements to form
    -Add gender radio buttons
    -Add save button
    -Add autosize area
    -Add username and email inputs

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -4,11 +4,19 @@ import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 
 export default function Map() {
+  function getCurrentPosition() {
+    navigator.geolocation.getCurrentPosition((position) => {
+      alert(`latitud:${position.coords.latitude}, longitud:${position.coords.longitude}`);
+    });
+  }
   return (
     <>
       <CssBaseline />
       <Container maxWidth="md">
-        <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '60vh' }} />
+        <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '60vh' }}>
+          <div id="map" />
+          <button type="button" onClick={getCurrentPosition}>pulsame</button>
+        </Typography>
       </Container>
     </>
   );
--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -1,28 +1,74 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { useState } from 'react';
-import TextField from '@material-ui/core/TextField';
+import {
+  TextField, TextareaAutosize, Radio, RadioGroup, FormControlLabel, FormControl, FormLabel, Button,
+} from '@material-ui/core';
+
 import './UserForm.css';
 
 export default function UserForm() {
   const [title, setTitle] = useState('');
   const [legend, setLegend] = useState('');
   const [errorTitle, setErrorTitle] = useState(false);
+  const [mailTitle, setMailTitle] = useState('');
+  const [mailLegend, setMailLegend] = useState('');
+  const [mailErrorTitle, setMailErrorTitle] = useState(false);
+  const [value, setValue] = React.useState('');
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
   return (
     <form className="UserForm">
-      <TextField
-        onChange={(event) => {
-          setTitle(event.target.value);
-          if (title.length > 2) {
-            setErrorTitle(true);
-            setLegend('Username is bigger than 2');
-          } else {
-            setErrorTitle(false);
-          }
-        }}
-        error={errorTitle}
-        label="Username"
-        helperText={legend}
-        variant="outlined"
-      />
+      <div>
+        <TextField
+          onChange={(event) => {
+            setTitle(event.target.value);
+            if (title.length > 2) {
+              setErrorTitle(true);
+              setLegend('Username is bigger than 2');
+            } else {
+              setErrorTitle(false);
+            }
+          }}
+          error={errorTitle}
+          label="Username"
+          helperText={legend}
+          variant="outlined"
+        />
+        <TextField
+          onChange={(event) => {
+            setMailTitle(event.target.value);
+            if (mailTitle.length > 2) {
+              setMailErrorTitle(true);
+              setMailLegend('Email is bigger than 2');
+            } else {
+              setMailErrorTitle(false);
+            }
+          }}
+          error={mailErrorTitle}
+          label="Email"
+          helperText={mailLegend}
+          variant="outlined"
+        />
+      </div>
+      <FormControl component="fieldset" className="RadioForm">
+        <FormLabel component="legend">Gender</FormLabel>
+        <RadioGroup aria-label="gender" name="gender1" value={value} onChange={handleChange} required="true">
+          <FormControlLabel value="female" control={<Radio />} label="Female" />
+          <FormControlLabel value="male" control={<Radio />} label="Male" />
+        </RadioGroup>
+      </FormControl>
+      <div>
+        <label htmlFor="Bio">
+          Bio
+          <TextareaAutosize aria-label="empty textarea" placeholder="Bio" />
+        </label>
+      </div>
+      <Button variant="contained" color="primary">
+        SAVE
+      </Button>
     </form>
+
   );
 }
